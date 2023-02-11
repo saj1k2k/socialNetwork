b92b5d754f5417755889e7bcb177c0ee4b5dbd6d
saj1k2k <jonikbarezzi3@gmail.com>
2023-02-11 19:25:07 +0300

    feat: little bit update interface

--- app/src/components/Navbar/Navbar.jsx
+++ app/src/components/Navbar/Navbar.jsx
@@ -6,15 +6,19 @@ const Navbar = () => {
         <div className={s.item}>
             <a>Profile</a>
         </div>
+
         <div className={s.item}>
             <a>Messages</a>
         </div>
+
         <div className={s.item}>
             <a>News</a>
         </div>
+
         <div className={s.item}>
             <a>Music</a>
         </div>
+        
         <div className={s.item}>
             <a>Settings</a>
         </div>
