da60e18777e2d52756e2c519eeed2d450119dd59
saj1k2k <jonikbarezzi3@gmail.com>
2023-02-13 22:12:20 +0300

    feat: some refactoring

--- app/src/index.js
+++ app/src/index.js
@@ -3,28 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
-let posts = [
-    {id:0, message:'Hi,how are u?', likesCount: 12},
-    {id:1, message:'Hi all', likesCount: 255},
-    {id:1, message:'Ку бомжи', likesCount: 5},
-    {id:1, message:'Ку бомжи', likesCount: 5},
-    {id:1, message:'Ку бомжи', likesCount: 5},
-    {id:1, message:'Ку бомжи', likesCount: 5},
-    {id:1, message:'Ку бомжи', likesCount: 5}
-
-]
-let dialogs = [
-    {id: 1, name: 'Petr'},
-    {id: 2, name: 'Andrey'}
-]
-let messages = [
-    {id:0, text:'Yo'},  
-    {id:1, text:'Hello my friend'}
-]
+import state from './redux/state'
 
 
-ReactDOM.render(<App posts={posts} dialogs={dialogs} messages={messages} />, document.getElementById('root'));
+ReactDOM.render(<App state = {state} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ app/src/redux/state.js
@@ -0,0 +1,28 @@
+let state = {
+    profilePage: {
+        posts:[
+            {id:0, message:'Hi,how are u?', likesCount: 12},
+            {id:1, message:'Hi all', likesCount: 255},
+            {id:1, message:'Ку бомжи', likesCount: 5},
+            {id:1, message:'Ку бомжи', likesCount: 5},
+            {id:1, message:'Ку бомжи', likesCount: 5},
+            {id:1, message:'Ку бомжи', likesCount: 5},
+            {id:1, message:'Ку бомжи', likesCount: 5}
+        
+        ],
+        
+    },
+    dialogsPage: {
+        messages:[
+            {id:0, text:'Yo'},  
+            {id:1, text:'Hello my friend'}
+        ],
+        dialogs:[
+            {id: 1, name: 'Petr'},
+            {id: 2, name: 'Andrey'}
+        ],
+    }
+}
+
+
+export default state;
