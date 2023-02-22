33b3082958ab663d4cbb7f542a794711372f9b14
saj1k2k <jonikbarezzi3@gmail.com>
2023-02-22 14:30:21 +0300

    feat: connect to API and add users

--- app/src/Users/Users.jsx
+++ app/src/Users/Users.jsx
@@ -1,68 +1,51 @@
 import React from 'react';
 import styles from './users.module.css';
+import axios from 'axios'
+import userImg from '../assets/image/userImg.png'
 
-let Users = (props) => {
-    if (props.users.length === 0) {
-        props.setUsers([
-                {
-                    id: 1,
-                    photoUrl: 'https://voennoe-obozrenie.ru/uploads/posts/2020-12/1609331713_dima-2.jpg',
-                    followed: false,
-                    fullName: 'Dmitry',
-                    status: 'I am a boss',
-                    location: {city: 'Minsk', country: 'Belarus'}
-                },
-                {
-                    id: 2,
-                    photoUrl: 'https://voennoe-obozrenie.ru/uploads/posts/2020-12/1609331713_dima-2.jpg',
-                    followed: true,
-                    fullName: 'Sasha',
-                    status: 'I am a boss too',
-                    location: {city: 'Moscow', country: 'Russia'}
-                },
-                {
-                    id: 3,
-                    photoUrl: 'https://voennoe-obozrenie.ru/uploads/posts/2020-12/1609331713_dima-2.jpg',
-                    followed: false,
-                    fullName: 'Andrew',
-                    status: 'I am a boss too',
-                    location: {city: 'Kiev', country: 'Ukraine'}
-                }
-            ]
-        )
+class Users extends React.Component {
+
+    constructor(props) {
+        super(props);
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            this.props.setUsers(response.data.items)
+        })
     }
 
-    return <div>
+    render () {
+        return <div>
         {
-            props.users.map(u => <div key={u.id}>
+            this.props.users.map(u => <div key={u.id}>
                 <span>
                     <div >
-                        <img src={u.photoUrl} className={styles.userPhoto}  alt='avatar'/>
+                        <img src={u.photos.small != null ? u.photos.small : userImg} className={styles.userPhoto}  alt='avatar'/>
                     </div>
                     <div>
                         {u.followed
                             ? <button onClick={() => {
-                                props.unfollow(u.id)
+                                this.props.unfollow(u.id)
                             }}>Unfollow</button>
                             : <button onClick={() => {
-                                props.follow(u.id)
+                                this.props.follow(u.id)
                             }}>Follow</button>}
 
                     </div>
                 </span>
                 <span>
                     <span>
-                        <div>{u.fullName}</div>
+                        <div>{u.name}</div>
                         <div>{u.status}</div>
                     </span>
                     <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
+                        <div>{"u.location.country"}</div>
+                        <div>{"u.location.city"}</div>
                     </span>
                 </span>
             </div>)
         }
     </div>
+    }
 }
+
 
 export default Users;
