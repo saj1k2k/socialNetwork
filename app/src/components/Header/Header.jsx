65bbbb576013bff3681d8bb3c044ff259dcf26ce
saj1k2k <jonikbarezzi3@gmail.com>
2023-02-25 00:18:17 +0300

    feat: set black styles and logo

--- app/src/components/Header/Header.jsx
+++ app/src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import s from './Header.module.css';
 
 const Header = () => {
     return <header className={s.header}>
-        <img src='https://www.freelogodesign.org/Content/img/logo-ex-7.png' />
+        <img src='https://cdn.discordapp.com/attachments/1041876464829747222/1078786420640055296/saj1k2k_minimalistic_logo_like_facebook_b395a1ad-4ab9-47a5-a85d-89fe3a193417.png' alt='rerere' />
     </header>
 }
 
