9608019988e288f21d463033de4fa8814491ef63
saj1k2k <jonikbarezzi3@gmail.com>
2023-02-25 00:18:50 +0300

    feat: styles

--- app/src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ app/src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -11,11 +11,10 @@ const ProfileInfo = (props) => {
         <div>
             <div>
                 <img
-                    src='https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350'/>
+                    src='https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350' alt='zatichka'/>
             </div>
             <div className={s.descriptionBlock}>
-                <img src={props.profile.photos.large} />
-                ava + description
+                <img src={props.profile.photos.large} alt='re' />   
             </div>
         </div>
     )
